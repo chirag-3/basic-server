29460ba988e393047430091d320da07a06ae4160
Chirag Lala <chirag42001@gmail.com>
2026-05-16 09:58:11 +0000

    A simple response response returned

--- main/main.go
+++ main/main.go
@@ -6,10 +6,19 @@ import (
 	"net/http"
 )
 
+// func helloHandler1() string {
+// 	return "Thi is hello page"
+// }
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello my name is Chirag")
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	fmt.Println("This is the main function")
 	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", helloHandler)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
