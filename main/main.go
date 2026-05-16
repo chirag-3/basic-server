d34f1412368674e2f821f0a49527d06c9a56f0da
Chirag Lala <chirag42001@gmail.com>
2026-05-16 11:24:18 +0000

    Adding a form to the server

--- main/main.go
+++ main/main.go
@@ -14,11 +14,19 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello my name is Chirag")
 }
 
+func detailsHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "%s stays at %s", name, address)
+}
+
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	fmt.Println("This is the main function")
 	http.Handle("/", fileServer)
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/submitDetails", detailsHandler)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
